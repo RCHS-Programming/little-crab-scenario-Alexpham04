4cbbd9a2f328b1e0367cc5e5e9d1966bdc397e67
Alexpham04 <Alexander.pham@stu.boone.kyschools.us>
2021-03-16 08:39:52 -0400

    added lobster

--- Crab.java
+++ Crab.java
@@ -4,27 +4,56 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
  * This class defines a crab. Crabs live on the beach.
  */
 public class Crab extends Actor
-{
+{   
     public void act()
     {
-        if (isAtEdge())
-        {
-            turn(17);
-        }
-        if( Greenfoot.getRandomNumber(100)<10)
-        {
-            turn(5);
-        }
+        
+        randomTurn();
         move();
-        if ( isTouching(Worm.class))
-        {
-            removeTouching(Worm.class);
-        }
+       lookforWorm();
     }
     public void move( )
     {
         move(5);
         
-    }              
+    }   
+    
+    
+    /*cheack if we found a Worm 
+     *if we have eat it 
+     * 
+     */
+    public void lookforWorm()
+    {
+            if ( isTouching(Worm.class))
+        {
+            removeTouching(Worm.class);
+              move(5);
+        }
+    }
+    
+    /*make the crab randomly turn left or right 
+     * 
+     */
+    public void randomTurn()
+    {
+        if( Greenfoot.getRandomNumber(100)<10)
+        {
+            turn( Greenfoot.getRandomNumber(91)-45);
+   
+            
+        }
+    }
+    /* do what the crab wants 
+     * 
+     */
+    public void turnAtEdge()
+    {
+        if (isAtEdge())
+        {
+            turn(17);
+            RandomTurn();
+        }      
+    }
 }
     
